e85a54d6e547c7ecc0b2a263c35c5d21833b76f9
fganga <fganga@unaj.edu.ar>
2023-09-07 21:51:54 -0300

    refactor: use a table for filtered dirs.

--- lua/go_tests.lua
+++ lua/go_tests.lua
@@ -35,11 +35,17 @@ end
 M.fuzzy_dirs= function (opts)
     opts = opts or {}
 
+    local filtered_dirs = {
+        "git",
+        "node",
+        "vendor",
+        "var"
+    }
+
     -- vim.fn.jobstart({"find", ".", "-type", "d", "|", "grep", "-vE", "git"}, {
         vim.fn.jobstart({"find", ".", "-type", "d"}, {
             stdout_buffered = true,
             on_stdout = function (_, data)
-
 
                 local d = data
 
@@ -52,40 +58,29 @@ M.fuzzy_dirs= function (opts)
                     end
                 end
 
-                table.filter(filtered, function(e)
-                    return string.find(e, "git") == nil
-                end)
-
-                table.filter(filtered, function(e)
-                    return string.find(e, "node") == nil
-                end)
-
-                table.filter(filtered, function(e)
-                    return string.find(e, "vendor") == nil
-                end)
-
-                table.filter(filtered, function(e)
-                    return string.find(e, "var") == nil
-                end)
-
-
-            pickers.new(opts, {
-                prompt_title = "colors",
-                finder = finders.new_table {
-                    results = filtered
-                },
-                sorter = conf.generic_sorter(opts),
-
-                attach_mappings = function(prompt_bufnr, map)
-                    actions.select_default:replace(function()
-                        actions.close(prompt_bufnr)
-                        local selection = action_state.get_selected_entry()
-                        vim.cmd(string.format("silent edit %s", selection[1]))
+                for i, dir in ipairs(filtered_dirs) do
+                    table.filter(filtered, function(e)
+                        return string.find(e, dir) == nil
                     end)
-                    return true
-                end,
+                end
 
-            }):find()
+                pickers.new(opts, {
+                    prompt_title = "colors",
+                    finder = finders.new_table {
+                        results = filtered
+                    },
+                    sorter = conf.generic_sorter(opts),
+
+                    attach_mappings = function(prompt_bufnr, map)
+                        actions.select_default:replace(function()
+                            actions.close(prompt_bufnr)
+                            local selection = action_state.get_selected_entry()
+                            vim.cmd(string.format("silent edit %s", selection[1]))
+                        end)
+                        return true
+                    end,
+
+                }):find()
         end
     })
 end
